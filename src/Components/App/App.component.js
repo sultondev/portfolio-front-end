b92c0371f37592ebd936b3177c2856cc6a423ebd
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-04-02 17:07:09 +0500

    initial commit

---
+++ src/Components/App/App.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.style.sass';
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app-header">
+				<h1>SultonDev</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
